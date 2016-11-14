11eddb33dfe88fd8311517ffa6249f40d3c0267f
Fredrik Welander <fredrik.welander@subsite.fi>
2016-11-14 23:06:54 +0200

    modified:   telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -18,6 +18,7 @@ import os
 import sys
 import socket
 import ConfigParser
+# request seems not to be installed by default on Debian/Ubuntu, check for it
 try:
 	import requests
 except ImportError:
@@ -25,10 +26,15 @@ except ImportError:
 	exit(1)
 
 usage = 'USAGE: telegrambot.py "MESSAGE"'
+hostname = socket.gethostname()
+
+# Init config
 config = ConfigParser.ConfigParser()   
 configFile = "/etc/telegrambot.conf"
 
+# Check if config file exists
 if len(config.read(configFile)) != 1:
+	# Check if run by root
 	if os.geteuid() == 0:
 		if raw_input("Run setup? [Y/n]: ").lower() not in [ 'y', '' ]:
 			exit(0)
@@ -50,23 +56,30 @@ if len(config.read(configFile)) != 1:
 		print "Config file {0} not found or no read permissions. Run as root for setup.".format(configFile)
 		exit(1)
 
+# Check for correct amount of args
 if len(sys.argv) < 2:
 	print usage
 	sys.exit(1)
 
-hostname = socket.gethostname()
+# Read api token from config
 token = config.get('main', 'api_token')
 message = "*{0}:* {1}".format(hostname, sys.argv[1])
 
+# Request function
 def botcall(method, data={}):
-	return requests.get(
+	response = requests.get(
 		url='https://api.telegram.org/bot{0}/{1}'.format(token, method),
 		params=data
 	).json()
-	return
+	if not response["ok"]:
+		print "Request error: {0} {1}".format(response["error_code"], response["description"])
+		exit(1)
+	return response
 
+# Request 1/2, get chat_id
 chat_id = botcall("getUpdates")["result"][-1]["message"]["chat"]["id"]
 
+# Request 2/2, send message
 botcall("sendMessage", {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"})
 
 
